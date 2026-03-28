00f29b81b7a49e87815053ddee762954f3c36572
abuhurairahbinamer <abuhurairah756@gmail.com>
2026-03-28 14:55:23 +0500

    second attempt

--- khudii-app/src/App.jsx
+++ khudii-app/src/App.jsx
@@ -1,42 +1,45 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense, lazy } from 'react'
 import { useLocation } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import Navbar from './componets/Navbar'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/Home/Home'
 import Topbar from './componets/Topbar/Topbar'
-import Organizations from './pages/Organizations/Organizations'
 import Footer from './componets/secondlast/Footer'
-import Organization_Detail from './pages/OrganizationDetail/Organization_Detail'
-import About from './pages/About/About'
-import BlogDetails from './pages/SpecificBlog/SpecificBlog'
 import VapiAssistant from './componets/VAPI.AI/Vapi'
-import Golden_people from './pages/GoldenPeople/Golden_people'
-import ContactUs from './pages/Contacts/ContactUs'
-import ContributeStory from './pages/ContributeStory/Contribute'
-import Donate from './pages/DonateNow/Donate'
-import VideoGallery from './componets/SuccessStoriesPageComponent/Success'
-import SocialMedai from './pages/SocialMedia/SocialMedai'
-import Vedios from './pages/Vedios/Vedios'
-import Testimonial from './pages/testimonial/Testimonial'
-import Tribute from './pages/Tribute/Tribute'
-import Certification from './pages/certifications/Certifications'
-import FAQSection from './pages/FAQs/FAQ'
-import Blogss from './pages/Blogs/Blogs'
-import JobApplicationForm from './pages/Jobs/Jobs'
-import VolunteerForm from './pages/Volunteer/Volunteer'
-import Categories from './pages/Categories/Categories'
 import BackToTopButton from './componets/backToTopButton/BackToTopButton.jsx'
 import SocialShare from './componets/SocialShare/SocialShare.jsx'
 import axios from 'axios'
 import './cache.js'
 import { useGoogleAnalytics } from './Hooks/GoogleAnalytics.jsx'
-import Detailforall from './pages/DetailforAll/Detailforall.jsx'
-import SuccessStoryDetail from './pages/Success-Stories-detail/SuccessStoryDetail.jsx'
-import Error from "./pages/Error/Error.jsx"
-import OrganizationForm from './componets/organizationform/orgForm.jsx'
-import AuthCallback from './pages/AuthCalBack/AuthCallBack.jsx'
+
+// Lazy loaded routes to improve performance and code-split the massive bundle
+const HomePage = lazy(() => import('./pages/Home/Home'));
+const Organizations = lazy(() => import('./pages/Organizations/Organizations'));
+const Organization_Detail = lazy(() => import('./pages/OrganizationDetail/Organization_Detail'));
+const About = lazy(() => import('./pages/About/About'));
+const BlogDetails = lazy(() => import('./pages/SpecificBlog/SpecificBlog'));
+const Golden_people = lazy(() => import('./pages/GoldenPeople/Golden_people'));
+const ContactUs = lazy(() => import('./pages/Contacts/ContactUs'));
+const ContributeStory = lazy(() => import('./pages/ContributeStory/Contribute'));
+const Donate = lazy(() => import('./pages/DonateNow/Donate'));
+const VideoGallery = lazy(() => import('./componets/SuccessStoriesPageComponent/Success'));
+const SocialMedai = lazy(() => import('./pages/SocialMedia/SocialMedai'));
+const Vedios = lazy(() => import('./pages/Vedios/Vedios'));
+const Testimonial = lazy(() => import('./pages/testimonial/Testimonial'));
+const Tribute = lazy(() => import('./pages/Tribute/Tribute'));
+const Certification = lazy(() => import('./pages/certifications/Certifications'));
+const FAQSection = lazy(() => import('./pages/FAQs/FAQ'));
+const Blogss = lazy(() => import('./pages/Blogs/Blogs'));
+const JobApplicationForm = lazy(() => import('./pages/Jobs/Jobs'));
+const VolunteerForm = lazy(() => import('./pages/Volunteer/Volunteer'));
+const Categories = lazy(() => import('./pages/Categories/Categories'));
+const Detailforall = lazy(() => import('./pages/DetailforAll/Detailforall.jsx'));
+const SuccessStoryDetail = lazy(() => import('./pages/Success-Stories-detail/SuccessStoryDetail.jsx'));
+const Error = lazy(() => import("./pages/Error/Error.jsx"));
+const OrganizationForm = lazy(() => import('./componets/organizationform/orgForm.jsx'));
+const AuthCallback = lazy(() => import('./pages/AuthCalBack/AuthCallBack.jsx'));
+
 const APIPath = import.meta.env.VITE_BACKEND_PATH;
 // import TranslationWidget from './componets/translate/Translate'
 function App() {
@@ -88,33 +91,40 @@ function App() {
       <main id="main-content">
         <Topbar />
         <Navbar />
-        <Routes>
-          <Route path='/' element={<HomePage con={findobj('/')} url={url} />} />
-          <Route path='/organizations' element={<Organizations con={findobj('/organizations')} url={url} />} />
-          {/* <Route path='/organization/:slug' element={<Organization_Detail url={url}/>} /> */}
-          <Route path='/about-khudii' element={<About con={findobj('/about-khudii')} url={url} />} />
-          {/* <Route path='/Blog/:slug' element={<BlogDetails url={url}/>}/> */}
-          <Route path='/golden-people' element={<Golden_people con={findobj('/golden-people')} url={url} />} />
-          <Route path='/contact' element={<ContactUs con={findobj('/contact')} url={url} />} />
-          <Route path='/contribute-your-story' element={<ContributeStory con={findobj('/contribute-your-story')} url={url} />} />
-          <Route path='/donate-now' element={<Donate con={findobj('/donate-now')} url={url} />} />
-          <Route path='/success-stories' element={<VideoGallery con={findobj('/success-stories')} url={url} />} />
-          <Route path='/social-media' element={<SocialMedai con={findobj('/social-media')} url={url} />} />
-          <Route path='/videos' element={<Vedios con={findobj('/videos')} url={url} />} />
-          <Route path='/testimonials' element={<Testimonial con={findobj('/testimonials')} url={url} />} />
-          <Route path='/tribute' element={<Tribute con={findobj('/tribute')} url={url} />} />
-          <Route path='/certifications' element={<Certification con={findobj('/certifications')} url={url} />} />
-          <Route path='/faqs' element={<FAQSection con={findobj('/faqs')} url={url} />} />
-          <Route path='/blogs' element={<Blogss con={findobj('/blogs')} url={url} />} />
-          <Route path='/jobs' element={<JobApplicationForm con={findobj('/jobs')} url={url} />} />
-          <Route path='/volunteer' element={<VolunteerForm con={findobj('/volunteer')} url={url} />} />
-          <Route path='/success-stories/:slug' element={<SuccessStoryDetail url={url} />} />
-          {/* <Route path='Categories/:slug' element={<Categories url={url}/>}/> */}
-          <Route path="/organization/registration" element={<OrganizationForm/>}/>
-          {/* <Route path="/auth-callback" element={<AuthCallback/>}/> */}
-          <Route path='/:slug' element={<Detailforall url={url} />} />
-          <Route path='/*' element={<Error url={url}/>}/>
-        </Routes>
+        {/* Render a simple fallback spinner while lazy components are downloading */}
+        <Suspense fallback={
+          <div className="flex w-full h-80 items-center justify-center">
+            <div className="w-10 h-10 border-4 border-[#e7001e] border-t-transparent rounded-full animate-spin"></div>
+          </div>
+        }>
+          <Routes>
+            <Route path='/' element={<HomePage con={findobj('/')} url={url} />} />
+            <Route path='/organizations' element={<Organizations con={findobj('/organizations')} url={url} />} />
+            {/* <Route path='/organization/:slug' element={<Organization_Detail url={url}/>} /> */}
+            <Route path='/about-khudii' element={<About con={findobj('/about-khudii')} url={url} />} />
+            {/* <Route path='/Blog/:slug' element={<BlogDetails url={url}/>}/> */}
+            <Route path='/golden-people' element={<Golden_people con={findobj('/golden-people')} url={url} />} />
+            <Route path='/contact' element={<ContactUs con={findobj('/contact')} url={url} />} />
+            <Route path='/contribute-your-story' element={<ContributeStory con={findobj('/contribute-your-story')} url={url} />} />
+            <Route path='/donate-now' element={<Donate con={findobj('/donate-now')} url={url} />} />
+            <Route path='/success-stories' element={<VideoGallery con={findobj('/success-stories')} url={url} />} />
+            <Route path='/social-media' element={<SocialMedai con={findobj('/social-media')} url={url} />} />
+            <Route path='/videos' element={<Vedios con={findobj('/videos')} url={url} />} />
+            <Route path='/testimonials' element={<Testimonial con={findobj('/testimonials')} url={url} />} />
+            <Route path='/tribute' element={<Tribute con={findobj('/tribute')} url={url} />} />
+            <Route path='/certifications' element={<Certification con={findobj('/certifications')} url={url} />} />
+            <Route path='/faqs' element={<FAQSection con={findobj('/faqs')} url={url} />} />
+            <Route path='/blogs' element={<Blogss con={findobj('/blogs')} url={url} />} />
+            <Route path='/jobs' element={<JobApplicationForm con={findobj('/jobs')} url={url} />} />
+            <Route path='/volunteer' element={<VolunteerForm con={findobj('/volunteer')} url={url} />} />
+            <Route path='/success-stories/:slug' element={<SuccessStoryDetail url={url} />} />
+            {/* <Route path='Categories/:slug' element={<Categories url={url}/>}/> */}
+            <Route path="/organization/registration" element={<OrganizationForm/>}/>
+            {/* <Route path="/auth-callback" element={<AuthCallback/>}/> */}
+            <Route path='/:slug' element={<Detailforall url={url} />} />
+            <Route path='/*' element={<Error url={url}/>}/>
+          </Routes>
+        </Suspense>
         <Footer />
         <VapiAssistant />
         <BackToTopButton />
